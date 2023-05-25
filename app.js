22bbaedf8d492c3ed0f1ec10c694e84b948c98ea
obregonpablo <obregonpablo2022@gmail.com>
2023-05-24 22:03:46 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+})
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/home.html'))
+})
+
+app.get('/login', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/login.html'))
+})
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.join(__dirname,'views/register.html'))
+})
+
